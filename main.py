8ef325c5c72fef66e62548a96c052c58cd39be8f
GH <130484247+vvzsf@users.noreply.github.com>
2023-12-12 09:53:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
-
-
+def start(update, context):
+    update.message.reply_text("Hello! Welcome to PanindiaFilmZ Main Bot.")
 
 updater telegram.ext.Updater (TOKEN, use_context = True)
 dispatch updater.dispatcher
